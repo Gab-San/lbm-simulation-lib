3f37d708543dbbe2faf46f3b07a97af6d618845d
Gabriele Santandrea <gabriele.santandrea@mail.polimi.it>
2026-08-13 11:37:38 +0200

    Feat/fullway to halfway (#6)
    * feat(boundaries): from fullway to halfway
    
    * feat(benchmarkVisualize): adjusted benchmark comparison to halfway boundary conditions

--- scripts/benchmarkVisualize.py
+++ scripts/benchmarkVisualize.py
@@ -1,12 +1,14 @@
 import sys
-import numpy as np
-import matplotlib.pyplot as plt
+
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def read_data(file_name, bench_name):
 
     # leggo i dati di ghia
-    with open(bench_name, 'r') as f:
+    with open(bench_name, "r") as f:
         ghia_data = []
         for line in f:
             x, y = line.strip().split()
@@ -20,7 +22,7 @@ def read_data(file_name, bench_name):
         ghia_data = [(x, y / max_value_ghia) for x, y in ghia_data]
 
     # leggo i dati della simulazione dal file
-    with open(file_name, 'rb') as f:
+    with open(file_name, "rb") as f:
         # reading velocities
         data = np.fromfile(f, dtype=np.float64)
 
@@ -29,29 +31,32 @@ def read_data(file_name, bench_name):
             data = data / max_value
 
         # max_value = max([abs(v) for v in data])
-        # data = [v / max_value for v in data]    
+        # data = [v / max_value for v in data]
 
     return data, ghia_data
+
 
 def create_graph(data, ghia_data):
     # creo un grafico per i valori di data e lo mostro
     plt.figure()
     # data parte da x=0 a x=1 con passo 1/(len(data)-1)
-    x = np.linspace(0, 1, len(data))
-    plt.plot(x, data, label='LBM Simulation')
+    N = len(data)
+    x = (np.arange(N) + 0.5) / N
+    plt.plot(x, data, label="LBM Simulation")
 
     # Aggiungo i dati di Ghia et al. 1982 per confronto
     ghia_data = np.array(ghia_data)
-    plt.plot(ghia_data[:, 0], ghia_data[:, 1], label='Ghia et al.', marker='s')
-    plt.xlabel('y coordinate')
-    plt.ylabel('u velocity')
-    plt.title('Comparison of u-velocity profiles')
+    plt.plot(ghia_data[:, 0], ghia_data[:, 1], label="Ghia et al.", marker="s")
+    plt.xlabel("y coordinate")
+    plt.ylabel("u velocity")
+    plt.title("Comparison of u-velocity profiles")
     plt.legend()
     plt.grid(True)
     plt.show()
 
-if __name__ == '__main__':
-    if(len(sys.argv) < 3):
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
         print(f"Usage: {sys.argv[0]} <input_file_1> <ghia_bench_data>")
         sys.exit()
 
