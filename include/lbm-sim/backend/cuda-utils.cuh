d4fa08d4127f00c647ebdc8107cd22d4a30f36d9
Gabriele Santandrea <gabriele.santandrea@mail.polimi.it>
2026-08-15 16:38:40 +0200

    refactor(cuda-solver): initial cuda solver rewrite

---
+++ include/lbm-sim/backend/cuda-utils.cuh
@@ -0,0 +1,20 @@
+#ifndef __LBM_SIM_BACKEND_CUDA_UTILS_HPP
+#define __LBM_SIM_BACKEND_CUDA_UTILS_HPP
+
+namespace lbm {
+
+// Error checks for async errors
+#define LBM_CUDA_CHECK(expr)                                                   \
+  do {                                                                         \
+    const cudaError_t _lbm_err = (expr);                                       \
+    if (_lbm_err != cudaSuccess) {                                             \
+      std::ostringstream _lbm_oss;                                             \
+      _lbm_oss << "CUDA ERROR " << cudaGetErrorString(_lbm_err) << " at "      \
+               << __FILE__ << " : " << __LINE__;                               \
+      throw std::runtime_error(_lbm_oss.str());                                \
+    }                                                                          \
+  } while (0)
+
+} // namespace lbm
+
+#endif //  __LBM_SIM_BACKEND_CUDA_UTILS_HPP
